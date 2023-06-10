36d65067d6b20ae3714047e2986909d7e5d25aa8
Zotoavina <zotoavinanantenaina@gmail.com>
2023-06-10 19:52:00 +0200

    création d'une utilitaire pour formatter les réponses des api

---
+++ helpers/response.helper.js
@@ -0,0 +1,16 @@
+module.exports = {
+    success : function(res, data,message){
+      res.json( {
+        code : 202,
+        data : data,
+        message : message
+      } )
+    },
+    error : function(res, message, code = 400 ){
+      res.json( {
+        code : code,
+        message : message
+      } )
+    }
+  }
+  
