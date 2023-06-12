d7428bbc347df851f287e6f740aae1973331c5d4
Finoana71 <finoanamandresy71@gmail.com>
2023-06-12 14:26:55 +0300

    Création service pour créer un assignement

---
+++ services/assignment.service.js
@@ -0,0 +1,28 @@
+const { ObjectId } = require("mongodb");
+const Assignment = require("../model/assignment");
+
+// creer assignment
+async function createAssignment(body){
+    console.log("POST assignment reçu :");
+    let assignment = createAssignmentObject(body); 
+    assignment = await assignment.save();
+    return assignment;
+}
+
+// Créer un objet assignment
+function createAssignmentObject({id, nom, dateDeRendu, rendu, note, remarques, eleve, matiere}){
+    let assignment = new Assignment();
+    assignment.id = id;
+    assignment.nom = nom;
+    assignment.dateDeRendu = dateDeRendu;
+    assignment.rendu = rendu;
+    assignment.note = note;
+    assignment.remarques = remarques;
+    assignment.eleve = ObjectId(eleve);
+    assignment.matiere = ObjectId(matiere);
+    return assignment;
+}
+
+module.exports = {
+    createAssignment
+}
