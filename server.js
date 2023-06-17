8c069a640dd681a9e359deb8c9c5e734bb89c8db
Zotoavina <zotoavinanantenaina@gmail.com>
2023-06-17 21:33:55 +0200

    accépter la methode PATCH dans le header des request

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ mongoose.connect(uri, options)
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, PATCH");
   next();
 });
 
